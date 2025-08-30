259c9e1217aaebbe859617e9b11b5762d698ef66
Magicolo <magicololand@gmail.com>
2025-08-30 05:14:05 +0000

    fix: Improve assertions and error messages in `sever` and `bail` functions for better clarity

--- phylactery/src/cell.rs
+++ phylactery/src/cell.rs
@@ -67,6 +67,7 @@ mod bind {
     }
 
     pub fn sever<T: ?Sized>(this: *const T, value: u32) -> bool {
+        debug_assert!(value > 0);
         let address = this.cast::<()>() as usize;
         match PANIC.with_borrow_mut(|map| map.insert(address, value)) {
             // This `Soul` is already unwinding. This can happen with a call to `Soul::sever`.
@@ -78,7 +79,7 @@ mod bind {
     pub fn bail<T: ?Sized>(this: *const T, drop: bool) -> bool {
         let address = this.cast::<()>() as usize;
         PANIC.with_borrow_mut(|map| match map.get_mut(&address) {
-            Some(0) => unreachable!("invalid state"),
+            Some(0) => unreachable!("invalid count"),
             Some(1) if drop => {
                 map.remove(&address);
                 true
@@ -95,18 +96,20 @@ mod bind {
 #[cfg(not(feature = "std"))]
 mod bind {
     use super::*;
-    use core::sync::atomic::{AtomicU32, AtomicUsize, Ordering};
+    use core::sync::atomic::{AtomicU32, AtomicUsize, Ordering::Relaxed};
 
     static PANIC: AtomicUsize = AtomicUsize::new(0);
     static COUNT: AtomicU32 = AtomicU32::new(0);
 
     pub fn sever<T: ?Sized>(this: *const T, value: u32) -> bool {
+        debug_assert!(value > 0);
         let address = this.cast::<()>() as usize;
-        match PANIC.compare_exchange(0, address, Ordering::Relaxed, Ordering::Relaxed) {
-            Ok(_) => match COUNT.compare_exchange(0, value, Ordering::Relaxed, Ordering::Relaxed) {
+        match PANIC.compare_exchange(0, address, Relaxed, Relaxed) {
+            Ok(_) => match COUNT.compare_exchange(0, value, Relaxed, Relaxed) {
                 Ok(_) => panic_lich_not_redeemed(value),
-                Err(_) => panic_multiple_unwind(),
+                Err(_) => unreachable!("invalid count"),
             },
+            // This `Soul` is already unwinding. This can happen with a call to `Soul::sever`.
             Err(panic) if panic == address => false,
             Err(_) => panic_multiple_unwind(),
         }
@@ -114,18 +117,16 @@ mod bind {
 
     pub fn bail<T: ?Sized>(this: *const T, drop: bool) -> bool {
         let address = this.cast::<()>() as usize;
-        match PANIC.load(Ordering::Relaxed) {
+        match PANIC.load(Relaxed) {
             0 => false,
-            panic if drop && panic == address => {
-                if COUNT.fetch_sub(1, Ordering::Relaxed) == 1 {
-                    match PANIC.compare_exchange(address, 0, Ordering::Relaxed, Ordering::Relaxed) {
-                        Ok(_) => true,
-                        Err(_) => panic_multiple_unwind(),
-                    }
-                } else {
-                    true
-                }
-            }
+            panic if drop && panic == address => match COUNT.fetch_sub(1, Relaxed) {
+                0 => unreachable!("invalid count"),
+                1 => match PANIC.compare_exchange(address, 0, Relaxed, Relaxed) {
+                    Ok(_) => true,
+                    Err(_) => panic_multiple_unwind(),
+                },
+                _ => true,
+            },
             panic if panic == address => true,
             _ => panic_multiple_unwind(),
         }
